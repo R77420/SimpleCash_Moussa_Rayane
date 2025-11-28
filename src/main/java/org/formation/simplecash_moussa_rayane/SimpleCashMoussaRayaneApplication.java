40216e86067f4cc7ff315b6780615258bed037ae
Rayane Moussa <rayane.moussa@epita.fr>
2025-11-28 14:13:40 +0100

    Initial commit

---
+++ src/main/java/org/formation/simplecash_moussa_rayane/SimpleCashMoussaRayaneApplication.java
@@ -0,0 +1,13 @@
+package org.formation.simplecash_moussa_rayane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCashMoussaRayaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCashMoussaRayaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/formation/simplecash_moussa_rayane/SimpleCashMoussaRayaneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.simplecash_moussa_rayane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCashMoussaRayaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
